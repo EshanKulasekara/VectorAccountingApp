bccffcfc2b1b22556051ff50d6194d438105df70
VectorIncorporation <vectorinc9@gmail.com>
2023-09-02 20:27:36 +0530

    second commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from rich.console import Console
+
+console = Console()
+
+def main():
+    while True:
+        console.print("Accounting App By Vector INC.", style="blue bold")
+        
+
+if __name__ == "__main__":
+    main()
